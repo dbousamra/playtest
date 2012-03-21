54a4eae1dadffb0ebf5812b52cc973692c005643
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2012-03-21 16:24:03 +1000

    Fixed dependency issue in plygins.scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,8 +7,16 @@ object ApplicationBuild extends Build {
   val appName = "playtest"
   val appVersion = "0.1"
 
+  resolvers += Resolver.url("Typesafe Ivy Releases", new java.net.URL("http://repo.typesafe.com/typesafe/ivy-snapshots/"))(Patterns("[organisation]/[module]/[revision]/ivy-[revision].xml"::Nil,
+    "[organisation]/[module]/[revision]/[artifact]-[revision].[ext]"::Nil,
+    false) )
+
   val appDependencies = Seq(
-    "postgresql" % "postgresql" % "8.4-702.jdbc4")
+    "postgresql" % "postgresql" % "8.4-702.jdbc4",
+	"play" %% "anorm" % "2.0-RC4")
+
+  
+
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here      
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,5 +4,12 @@ logLevel := Level.Warn
 // The Typesafe repository
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+resolvers ++= Seq(Resolver.url("Play Ivy Repo", new java.net.URL("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns))
+
+resolvers += "Anorm" at "http://download.playframework.org/ivy-releases/"
+
+libraryDependencies ++= Seq("play" %% "anorm" % "2.0-RC1-SNAPSHOT")
+
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % Option(System.getProperty("play.version")).getOrElse("2.0"))
+
